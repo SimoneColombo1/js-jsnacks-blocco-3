fb118014f6f03e740a357eb4934d52d8dda06aa6
SimoneColombo1 <simcolombo2@gmail.com>
2024-05-14 23:39:23 +0200

    creato un if else per stampare le ultime 5 cifre dell'array

--- array_tail/JS/script.js
+++ array_tail/JS/script.js
@@ -1,14 +1,27 @@
-let tail= [];
+const tail= [];
 const Nutente= Number.parseInt(prompt('inserisci un numero'));
 
-for(let i; i<Nutente; i++){
+for(let i=0; i < Nutente; i++){
 
    tail.push(Math.floor (Math.random() *101));
-
-   if(Nutente>5){
-
-    
-   }
-
+   
+   
+   
+ 
 
 }
+
+
+if(Nutente>5){
+    let print= tail.slice(-5);
+
+
+    console.log(print);
+
+
+  }
+  else{
+    console.log('numero troppo piccolo inserirne uno più grande');
+  }
+  
+ 
