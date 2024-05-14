08c2a85bfcbee4ed319ca65dfd3a67fe4a36ba18
SimoneColombo1 <simcolombo2@gmail.com>
2024-05-14 23:34:54 +0200

    creato l'array, il prompt e il ciclo flor per inserire un numero casuale

---
+++ array_tail/JS/script.js
@@ -0,0 +1,14 @@
+let tail= [];
+const Nutente= Number.parseInt(prompt('inserisci un numero'));
+
+for(let i; i<Nutente; i++){
+
+   tail.push(Math.floor (Math.random() *101));
+
+   if(Nutente>5){
+
+    
+   }
+
+
+}
