63dd3e8b44de16b64d50f15bb0406917049218f0
SimoneColombo1 <simcolombo2@gmail.com>
2024-05-15 12:42:58 +0200

    snack 11

---
+++ snack-11/JS/script.js
@@ -0,0 +1,11 @@
+let Array=[];
+let sum= 0;
+while( sum < 50){
+let NumUtente= Number.parseInt(prompt('inserisci un numero'));
+Array.push(NumUtente);
+sum = sum +NumUtente;
+
+
+}
+
+console.log(Array);
