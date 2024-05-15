0e9f069b7e39f3dee28e0f49ce98dc518a76a489
SimoneColombo1 <simcolombo2@gmail.com>
2024-05-15 12:20:51 +0200

    aggiunto un if else capire quale array sia più grande

--- snack-10/JS/script.js
+++ snack-10/JS/script.js
@@ -1,9 +1,18 @@
 let arrayA= [8, 7, 4,3, 1, 0, 4];
 let arrayB= [3,4,5,1,3,1,3,5,1,41,4,1313,3];
+if(arrayA.length < arrayB.length){
 
-while (arrayA.length < arrayB.length){
-
+    while (arrayA.length < arrayB.length){
+  
     arrayA.push(Number.parseInt(Math.random()*10));
+}}
+else{
+    while (arrayB.length < arrayA.length){
+  
+        arrayA.push(Number.parseInt(Math.random()*10));
+}
+
+
 }
 
 console.log(arrayA, arrayB);
