b8bf3adf641c82718d381134b4025f6d10768504
SimoneColombo1 <simcolombo2@gmail.com>
2024-05-22 12:46:32 +0200

    snack 15

---
+++ snack-15/JS/script.js
@@ -0,0 +1,13 @@
+let palla= {
+    'nome':"palla",
+   'Peso':"10"
+}
+
+
+
+let Nutente= Number.parseInt (prompt('inserisci il peso della palla')); 
+
+
+palla.peso= Nutente;
+
+console.log(palla.peso);
