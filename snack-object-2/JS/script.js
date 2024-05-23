c9905ea5ad1907867c3616e4909e0c4fc589d5ad
SimoneColombo1 <simcolombo2@gmail.com>
2024-05-23 12:37:02 +0200

    creato l'array persone

---
+++ snack-object-2/JS/script.js
@@ -0,0 +1,44 @@
+//& array persone
+
+let persone= [ 
+    {nome:"gino",
+        cognome: "ginetto",
+        eta:"15"
+    },
+    {nome:"carola",
+    cognome: "rolandi",
+    eta:"45"
+}, 
+{nome:"ivan",
+cognome: "ilterribile",
+eta:"12"
+},
+{nome:"sauro",
+cognome: "ermonnezza",
+eta:"55"
+},
+{nome:"rita",
+cognome: "hiffilger",
+eta:"22"
+},
+{nome:"piero",
+cognome: "peretti",
+eta:"21"
+},
+{nome:"gino",
+cognome: "strada",
+eta:"18"
+},
+{nome:"alberta",
+cognome: "cisente",
+eta:"87"
+},
+{nome:"giorgio",
+cognome: "giorgietti",
+eta:"34"
+},
+{nome:"gina",
+cognome: "labambina",
+eta:"4"
+},
+];
