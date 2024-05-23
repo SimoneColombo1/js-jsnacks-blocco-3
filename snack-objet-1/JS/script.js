c30237815724a6f28b8b1b87496a49fed1658346
SimoneColombo1 <simcolombo2@gmail.com>
2024-05-23 12:13:56 +0200

    dichiarato l'array

---
+++ snack-objet-1/JS/script.js
@@ -0,0 +1,70 @@
+//$array automobili
+
+let automobili= [ 
+{  marca:'opel',
+    modello:'drinzi',
+    alimentazione: 'gpl'
+},
+
+{  marca:'fiat',
+    modello:'fiatella',
+    alimentazione: 'benzina'
+},
+{  marca:'lamborghini',
+    modello:'tamarro',
+    alimentazione: 'metano'
+},
+{  marca:'ferrari',
+    modello:'schianto-130',
+    alimentazione: 'elettrico'
+},
+{  marca:'fiat',
+    modello:'panda rosso',
+    alimentazione: 'gpl'
+},
+{  marca:'pino',
+    modello:'carro armato',
+    alimentazione: 'metano'
+},
+{  marca:'opel',
+    modello:'open',
+    alimentazione: 'elettrico'
+},
+{  marca:'renault',
+    modello:'rino',
+    alimentazione: 'metano'
+},
+{  marca:'peugeot',
+    modello:'pidgeon(pokemon molto sottovalutato)',
+    alimentazione: 'benzina'
+},
+{  marca:'fiat',
+    modello:'punto e basta',
+    alimentazione: 'diesel'
+},
+{  marca:'jeep',
+    modello:'chip',
+    alimentazione: 'metano'
+},
+{  marca:'hyundai',
+    modello:'manontornai',
+    alimentazione: 'gpl'
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
