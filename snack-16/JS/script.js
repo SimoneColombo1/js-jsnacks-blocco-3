c26ca0d730d3aae1e7b3428f0df2502276f49d74
SimoneColombo1 <simcolombo2@gmail.com>
2024-05-24 11:21:24 +0200

    creato l'array zucchine

---
+++ snack-16/JS/script.js
@@ -0,0 +1,51 @@
+ 
+ 
+ //? array zucchine
+ 
+ const Zucchine=[
+    
+    { Varieta: 'Varietà 1',
+        peso:2,
+        lunghezza:4,
+    },
+    
+    { Varieta: 'varietà 2',
+    peso:6,
+    lunghezza:9,
+},
+{ Varieta: 'varietà 3',
+        peso:7,
+        lunghezza:15,
+    },
+    { Varieta: 'varietà 4',
+        peso:6,
+        lunghezza:3,
+    },
+    { Varieta: 'varietà 5',
+        peso:4,
+        lunghezza:7,
+    },
+    { Varieta: 'varietà 1',
+        peso:6,
+        lunghezza:12,
+    },
+    { Varieta: 'varietà 3',
+        peso:4,
+        lunghezza:14,
+    },
+    { Varieta: 'varietà 1',
+        peso:1,
+        lunghezza:4,
+    },
+    { Varieta: 'varietà 4',
+        peso:5,
+        lunghezza:6,
+    },
+
+
+
+
+
+]
+
+
